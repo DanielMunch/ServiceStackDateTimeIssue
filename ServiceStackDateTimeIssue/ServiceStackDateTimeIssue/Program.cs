449780436ef40f1e06c97560cc7698fb6e37fad3
DanielMunch <dm@safetrack.dk>
2014-09-05 09:57:28 +0200

    Demo project describing the datetime issue we are experiencing.

---
+++ ServiceStackDateTimeIssue/ServiceStackDateTimeIssue/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using ServiceStack;
+
+namespace ServiceStackDateTimeIssue
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(GetResponseJson());
+            Console.Read();
+        }
+
+        private static string GetResponseJson()
+        {            
+            var request = WebRequest.Create("http://localhost:60577/json/reply/GetDateTime?DateTime=2014-08-27 14:30:23");
+            request.Method = HttpMethods.Get;
+
+            JObject response;
+            using (var res = request.GetResponse())
+                response = JObject.Parse(res.GetResponseString());
+
+            return response["Value"].Value<string>();
+        }
+    }
+
+    public static class WebResponseExtensions
+    {
+        public static string GetResponseString(this WebResponse response)
+        {
+            using (var stream = response.GetResponseStream())
+            {
+                if (stream == null)
+                    return string.Empty;
+
+                using (var reader = new StreamReader(stream))
+                    return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ ServiceStackDateTimeIssue/ServiceStackServer/ServiceStackServer/DateTimeService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ServiceStack;
+
+namespace ServiceStackServer
+{
+    public class DateTimeService : Service
+    {
+        public object Any(GetDateTime request)
+        {
+            return new GetDateTimeResponse
+            {
+                Value = string.Format("{0}-{1}-{2} {3}:{4}:{5}", 
+                request.DateTime.Year, 
+                request.DateTime.Month, 
+                request.DateTime.Day, 
+                request.DateTime.Hour, 
+                request.DateTime.Minute, 
+                request.DateTime.Second)
+            };
+        }
+    }
+
+    public class GetDateTime : IReturn<GetDateTimeResponse>
+    {
+        public DateTime DateTime { get; set; }
+    }
+
+    public class GetDateTimeResponse
+    {
+        public string Value { get; set; }
+    }
+}
