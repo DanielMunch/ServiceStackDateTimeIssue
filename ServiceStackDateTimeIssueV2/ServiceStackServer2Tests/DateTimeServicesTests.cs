9c27d023521c9aba57c74d7a27f38f4ef605a34c
DanielMunch <dm@safetrack.dk>
2016-08-04 13:28:34 +0200

    more tests

--- ServiceStackDateTimeIssueV2/ServiceStackServer2Tests/DateTimeServicesTests.cs
+++ ServiceStackDateTimeIssueV2/ServiceStackServer2Tests/DateTimeServicesTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
+using Microsoft.SqlServer.Management.Smo;
 using ServiceStackServer2;
 
 namespace ServiceStackServer2Tests
@@ -22,5 +23,55 @@ namespace ServiceStackServer2Tests
 
         //    dt.Year.Should().Be(2017);
         //}
+
+        [Test]
+        public void ConnectionTestLocalHost()
+        {
+            var server = new Server();
+            server.ConnectionContext.ConnectionString = "Data Source=(local);Initial Catalog=master;Integrated security=true";
+            var db = new Database(server, "master");
+            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
+            r.Should().NotBe(null);
+        }
+
+        [Test]
+        public void ConnectionTestDev02Ip15()
+        {
+            var server = new Server();
+            server.ConnectionContext.ConnectionString = "Server=192.168.128.15,1433;Database=FMS;User ID=damu;Password=pXfeN5wT;Max Pool Size=1024";
+            var db = new Database(server, "master");
+            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
+            r.Should().NotBe(null);
+        }
+
+        [Test]
+        public void ConnectionTestProdIp51()
+        {
+            var server = new Server();
+            server.ConnectionContext.ConnectionString = "Server=192.168.128.51,1433;Database=FMS;User ID=ikasEhuFAmiZ;Password=UQy4EZYdaZY2;Max Pool Size=1024";
+            var db = new Database(server, "master");
+            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
+            r.Should().NotBe(null);
+        }
+
+        [Test]
+        public void ConnectionTestAzureProd()
+        {
+            var server = new Server();
+            server.ConnectionContext.ConnectionString = "Server=13.94.159.4,1433;;Database=FMS;User ID=ikasEhuFAmiZ;Password=UQy4EZYdaZY2;Max Pool Size=1024";
+            var db = new Database(server, "master");
+            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
+            r.Should().NotBe(null);
+        }
+
+        [Test]
+        public void ConnectionTestAzureDev()
+        {
+            var server = new Server();
+            server.ConnectionContext.ConnectionString = "Server=13.94.159.4,1434;Database=FMS;User ID=damu;Password=pXfeN5wT;Max Pool Size=1024";
+            var db = new Database(server, "master");
+            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
+            r.Should().NotBe(null);
+        }
     }
 }
