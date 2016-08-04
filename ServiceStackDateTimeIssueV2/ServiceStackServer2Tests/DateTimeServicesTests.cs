738957d6fa959c97b366ee685cc935ef6474aae3
DanielMunch <dm@safetrack.dk>
2016-08-04 13:51:05 +0200

    reformat tests

--- ServiceStackDateTimeIssueV2/ServiceStackServer2Tests/DateTimeServicesTests.cs
+++ ServiceStackDateTimeIssueV2/ServiceStackServer2Tests/DateTimeServicesTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Data;
+using FluentAssertions;
 using NUnit.Framework;
 using Microsoft.SqlServer.Management.Smo;
 using ServiceStackServer2;
@@ -27,51 +28,51 @@ namespace ServiceStackServer2Tests
         [Test]
         public void ConnectionTestLocalHost()
         {
-            var server = new Server();
-            server.ConnectionContext.ConnectionString = "Data Source=(local);Initial Catalog=master;Integrated security=true";
-            var db = new Database(server, "master");
-            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
-            r.Should().NotBe(null);
+            using (var connection = new System.Data.SqlClient.SqlConnection("Data Source=(local);Initial Catalog=master;Integrated security=true;Connection Timeout=5;"))
+            {
+                connection.Open();
+                connection.State.Should().Be(ConnectionState.Open);
+            }
         }
 
         [Test]
         public void ConnectionTestDev02Ip15()
         {
-            var server = new Server();
-            server.ConnectionContext.ConnectionString = "Server=192.168.128.15,1433;Database=FMS;User ID=damu;Password=pXfeN5wT;Max Pool Size=1024";
-            var db = new Database(server, "master");
-            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
-            r.Should().NotBe(null);
+            using (var connection = new System.Data.SqlClient.SqlConnection("Server=192.168.128.15,1433;Database=FMS;User ID=damu;Password=pXfeN5wT;Max Pool Size=1024;Connection Timeout=5;"))
+            {
+                connection.Open();
+                connection.State.Should().Be(ConnectionState.Open);
+            }
         }
 
         [Test]
         public void ConnectionTestProdIp51()
         {
-            var server = new Server();
-            server.ConnectionContext.ConnectionString = "Server=192.168.128.51,1433;Database=FMS;User ID=ikasEhuFAmiZ;Password=UQy4EZYdaZY2;Max Pool Size=1024";
-            var db = new Database(server, "master");
-            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
-            r.Should().NotBe(null);
+            using (var connection = new System.Data.SqlClient.SqlConnection("Server=192.168.128.51,1433;Database=FMS;User ID=ikasEhuFAmiZ;Password=UQy4EZYdaZY2;Max Pool Size=1024;Connection Timeout=5;"))
+            {
+                connection.Open();
+                connection.State.Should().Be(ConnectionState.Open);
+            }
         }
 
         [Test]
         public void ConnectionTestAzureProd()
         {
-            var server = new Server();
-            server.ConnectionContext.ConnectionString = "Server=13.94.159.4,1433;;Database=FMS;User ID=ikasEhuFAmiZ;Password=UQy4EZYdaZY2;Max Pool Size=1024";
-            var db = new Database(server, "master");
-            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
-            r.Should().NotBe(null);
+            using (var connection = new System.Data.SqlClient.SqlConnection("Server=13.94.159.4,1433;;Database=FMS;User ID=ikasEhuFAmiZ;Password=UQy4EZYdaZY2;Max Pool Size=1024;Connection Timeout=5;"))
+            {
+                connection.Open();
+                connection.State.Should().Be(ConnectionState.Open);
+            }
         }
 
         [Test]
         public void ConnectionTestAzureDev()
         {
-            var server = new Server();
-            server.ConnectionContext.ConnectionString = "Server=13.94.159.4,1434;Database=FMS;User ID=damu;Password=pXfeN5wT;Max Pool Size=1024";
-            var db = new Database(server, "master");
-            var r = db.ExecuteWithResults("SELECT * FROM sys.[databases]");
-            r.Should().NotBe(null);
+            using (var connection = new System.Data.SqlClient.SqlConnection("Server=13.94.159.4,1434;Database=FMS;User ID=damu;Password=pXfeN5wT;Max Pool Size=1024;Connection Timeout=5;"))
+            {
+                connection.Open();
+                connection.State.Should().Be(ConnectionState.Open);
+            }
         }
     }
 }
